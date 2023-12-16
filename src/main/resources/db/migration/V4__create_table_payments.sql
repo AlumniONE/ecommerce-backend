bdca26d581a82e90ff329edd80cdb48e35c7a736
Benjamin <admin@dataindev.com>
2023-12-16 20:43:14 -0300

    :recycle: Refactor Migrations

--- src/main/resources/db/migration/V4__create_table_payments.sql
+++ src/main/resources/db/migration/V4__create_table_payments.sql
@@ -1,4 +1,5 @@
-CREATE TABLE payments(
-                         id_payment BIGINT AUTO_INCREMENT PRIMARY KEY,
-                         payment_type VARCHAR(100) NOT NULL
+CREATE TABLE payments
+(
+    id_payment   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    payment_type VARCHAR(100) NOT NULL
 );
