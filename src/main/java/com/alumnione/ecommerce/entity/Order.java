050faf578eebcd6d662838441f2c3cb5bfbec1e3
Benjamin <admin@dataindev.com>
2023-12-17 16:32:49 -0300

    :money_with_wings: Add PaymentService & IPaymentService

--- src/main/java/com/alumnione/ecommerce/entity/Order.java
+++ src/main/java/com/alumnione/ecommerce/entity/Order.java
@@ -16,30 +16,30 @@ import lombok.Setter;
 @NoArgsConstructor
 
 public class Order {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id_order")
-  private Long orderId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order")
+    private Long orderId;
 
-  @ManyToOne
-  @JoinColumn(name = "user_id", referencedColumnName = "id_user")
-  private User user;
+    @Column(name = "order_status")
+    private String orderStatus;
 
-  @ManyToMany
-  @JoinTable(
+    @Column(name = "order_created_at")
+    private String orderCreatedAt;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Invoice invoice;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id_user")
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
     name = "cellphones",
     joinColumns = @JoinColumn(name = "product_id"),
     inverseJoinColumns = @JoinColumn(name = "id")
-  )
-  private List<Cellphone> productId;
-
-  @Column(name = "order_status")
-  private String orderStatus;
-
-  @Column(name = "order_created_at")
-  private String orderCreatedAt;
-
-  @OneToOne(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-  private Invoice invoice;
+    )
+    private List<Cellphone> productId;
 
 }
