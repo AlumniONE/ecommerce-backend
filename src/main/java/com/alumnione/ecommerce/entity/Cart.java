d229ed27ea67e960932e514606ae129b7e3ca01b
GutengMorgen <crisstian2345@gmail.com>
2023-12-21 22:00:14 -0500

    add ManyToMany relationship for Cart

--- src/main/java/com/alumnione/ecommerce/entity/Cart.java
+++ src/main/java/com/alumnione/ecommerce/entity/Cart.java
@@ -1,5 +1,6 @@
 package com.alumnione.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "carts")
@@ -20,12 +22,18 @@ public class Cart {
     @Column(nullable = false)
     private Long id;
 
-    // TODO: Agregar relación con Cellphone
-    // private Map<Cellphone, Integer> cellphones;
+    @ManyToMany
+    @JoinTable(
+            name="cart_cell",
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "cellphone_id")
+    )
+    private Set<Cellphone> cellphones;
 
     @Column(name = "last_updated")
     private LocalDateTime lastUpdated;
 
+    @JsonIgnore //error (user -> order -> invoice) SQL Error: 1146, SQLState: 42S02, Table 'ecommerce_backend_local.invoice' doesn't exist
     @OneToOne(orphanRemoval = true)
     @JoinColumn(name = "user_id")
     private User user;
