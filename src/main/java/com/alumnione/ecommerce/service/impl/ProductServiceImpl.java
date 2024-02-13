31a9ee784a7fb97fc34d17f5783581c789615ada
Benjamin <admin@dataindev.com>
2024-02-13 15:06:44 -0300

    :art: Add CrudService interface for implements on ServiceImpl classes

--- src/main/java/com/alumnione/ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/com/alumnione/ecommerce/service/impl/ProductServiceImpl.java
@@ -3,18 +3,17 @@ package com.alumnione.ecommerce.service.impl;
 import com.alumnione.ecommerce.dto.ProductDto;
 import com.alumnione.ecommerce.entity.Product;
 import com.alumnione.ecommerce.repository.ProductRepository;
-import com.alumnione.ecommerce.service.ProductService;
+import com.alumnione.ecommerce.service.CrudService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ProductServiceImpl implements ProductService {
+public class ProductServiceImpl implements CrudService<ProductDto,Product> {
 
     private final ProductRepository productRepository;
 
@@ -37,8 +36,8 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public ResponseEntity<Product> update(Long id, ProductDto productDto) {
-        if (id > 0 && productRepository.existsById(id)){
+    public ResponseEntity<String> update(Long id, ProductDto productDto) {
+        if (id > 0 && productRepository.existsById(id)) {
 
             var productUpdate = Product.builder()
             .id(id)
@@ -47,8 +46,9 @@ public class ProductServiceImpl implements ProductService {
             .price(productDto.price())
             .stock(productDto.stock())
             .build();
+            productRepository.save(productUpdate);
 
-            return new ResponseEntity<>(productRepository.save(productUpdate), HttpStatus.OK);
+            return new ResponseEntity<>("Product Updated",HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
